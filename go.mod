027b83d6130b97169b5d7f7fdfa8ac8b2c84d865
Carson Case <carsoncase@gmail.com>
2023-10-13 20:02:28 -0700

    server_test Fails

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.20
 require github.com/joho/godotenv v1.5.1
 
 require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-chi/cors v1.2.1
 	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.8.4
 )
--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+func setupRouter() *chi.Mux {
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	return router
+}
